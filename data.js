a4c26e5ebca7492c189be0a1a875edba0e599250
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-01 20:45:04 +0100

    fix typo

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ export const posts = [
         imageDesc: "photo of laptop with programming code",
         date: "March 1, 2023",
         title: "Publishing my learning journal",
-        summary: "After several months of learning about Frontend Web Developmente, I've finally published this journal to record the many projects I've completed on this path.",
+        summary: "After several months of learning about Frontend Web Development, I've finally published this journal to record the many projects I've completed on this path.",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
